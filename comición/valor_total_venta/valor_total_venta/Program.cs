172731a3833f1beb83fe2933b9ace7e671b02204
alejandro lemos gomez <alejandrolemosgomez@hotmail.com>
2024-09-13 21:39:45 -0500

    creando punto 1, cambios hechos

--- comición/valor_total_venta/valor_total_venta/Program.cs
+++ comición/valor_total_venta/valor_total_venta/Program.cs
@@ -3,6 +3,8 @@ public class Program
 {
     private static void Main(string[] args)
     {
+
+        double mayor,menor;
         // Solicitar el sueldo base
         Console.Write("Ingrese el sueldo base del vendedor: ");
         double sueldoBase = Convert.ToDouble(Console.ReadLine());
@@ -31,5 +33,22 @@ public class Program
         // Mostrar el resultado
         Console.WriteLine("\nTotal de comisiones: $" + totalComisiones);
         Console.WriteLine("Total que recibirá el vendedor: $" + totalRecibido);
+
+        //cual fue la venta que recibio mayor comision
+        double mayorComision = Math.Max(comision1, Math.Max(comision2, comision3));
+
+        if (mayorComision == comision1)
+        {
+            Console.WriteLine($"La venta que recibió mayor comisión es la primera con  un monto: ${venta1}");
+        }
+        else if (mayorComision == comision2)
+        {
+            Console.WriteLine($"La venta que recibió mayor comisión es la segunda con  un monto: ${venta2}");
+        }
+        else
+        {
+            Console.WriteLine($"La venta que recibió mayor comisión es la tercera con un  monto: ${venta3}");
+        }
     }
 }
+  
